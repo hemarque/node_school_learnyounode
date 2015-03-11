88b3a42ca2985b2890fd4fffc1975ffcd66f8142
hemarque <hemarque@gmail.com>
2015-03-11 11:46:27 +0100

    http collect

--- program.js
+++ program.js
@@ -4,7 +4,17 @@ var url = process.argv[2];
 
 http.get(url, function(resp){
   resp.setEncoding("utf8");
+
+  var buffer = "";
+  var characters = 0;
+
   resp.on("data", function(data){
-    console.log(data);
+    buffer += data;
+    characters += data.length;
+  });
+ 
+  resp.on("end", function(end){
+    console.log(characters);
+    console.log(buffer);
   });
 });
