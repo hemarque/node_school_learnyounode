d98df691e1d79103723dbbb2369781a3332bd6eb
hemarque <hemarque@gmail.com>
2015-03-12 16:32:25 +0100

    tcp time server

--- program.js
+++ program.js
@@ -1,33 +1,14 @@
-var http = require('http')
+var net = require('net');
 
-var urls = process.argv.slice(2);
-var strs = []; 
-var counter = 0;
-
-function work(id){
-  http.get(urls[id], function(res){
-    res.setEncoding('utf8');
-    var allData = ""; 
-    counter++;
-    res.on('data', function(data){
-      allData += data;    
-    }); 
-    res.on('end', function(){
-      counter--;
-      strs[id] = allData;
-      if(counter === 0)   
-        print();    
-      })  
-    }).on('error', function(e){
-      console.log("error:" + e.message);
-    })  
+function writeDate(date){
+  date.setHours(date.getHours()+1);
+  return date.toISOString().replace('T', ' ').substring(0,16);;
 }
 
-for(var i = 0; i < urls.length; i++){
-  work(i)
-}
+var server = net.createServer(function(socket){
+  var now = new Date();
+  socket.write(writeDate(now));
+  socket.end();
+});
 
-function print(){
-  for(var i = 0; i < strs.length; i++)
-    console.log(strs[i])
-}
+server.listen(process.argv[2]);
