9a1994d635a4ac7da1c33850ce95faac0495a892
hemarque <hemarque@gmail.com>
2015-03-12 14:16:02 +0100

    asyn http get with 3 urls

--- program.js
+++ program.js
@@ -1,20 +1,33 @@
-var http = require('http');
+var http = require('http')
 
-var url = process.argv[2];
+var urls = process.argv.slice(2);
+var strs = []; 
+var counter = 0;
 
-http.get(url, function(resp){
-  resp.setEncoding("utf8");
+function work(id){
+  http.get(urls[id], function(res){
+    res.setEncoding('utf8');
+    var allData = ""; 
+    counter++;
+    res.on('data', function(data){
+      allData += data;    
+    }); 
+    res.on('end', function(){
+      counter--;
+      strs[id] = allData;
+      if(counter === 0)   
+        print();    
+      })  
+    }).on('error', function(e){
+      console.log("error:" + e.message);
+    })  
+}
 
-  var buffer = "";
-  var characters = 0;
+for(var i = 0; i < urls.length; i++){
+  work(i)
+}
 
-  resp.on("data", function(data){
-    buffer += data;
-    characters += data.length;
-  });
- 
-  resp.on("end", function(end){
-    console.log(characters);
-    console.log(buffer);
-  });
-});
+function print(){
+  for(var i = 0; i < strs.length; i++)
+    console.log(strs[i])
+}
